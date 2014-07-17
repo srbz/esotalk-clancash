502d785ab511b175cfd007a7b507ec8e63c46f3c
srbz <srbz@hotmail.de>
2014-07-17 19:22:16 +0200

    on the way of being a plugin

--- plugin.php
+++ plugin.php
@@ -20,33 +20,51 @@ class ETPlugin_ClanCash extends ETPlugin
     public function __construct($rootDirectory)
     {
         parent::__construct($rootDirectory);
+        ETFactory::register("transactionModel", "TransactionModel", dirname(__FILE__)."/TransactionModel.class.php");
+        ETFactory::registerAdminController("transaction", "TransactionController", dirname(__FILE__)."/TransactionController.class.php");
 
     }
     public function handler_init($sender)
     {
 
     }
-    public function handler_initAdmin($sender)
+    public function handler_initAdmin($sender, $menu)
     {
-
+        $menu->add("transaction", "<a href='".URL("admin/transaction")."'><i class='icon-pencil'></i> ".T("Transaction")."</a>");
     }
-    
+
     public function setup($oldVersion = "")
     {
         //TODO use version to compare to old verisons and follow-up tasks
-        //TODO manipulate db schema and init filesystem structues
+        //db structure object
         $structure = ET::$database->structure();
-        $structure->table("eso_clancash")
-            ->column("id", "int(11) unsigned", false)
-            ->column("")
-        return true;
+        //if there is no table with name "eso_clancash"
+        if(!$structure->table("eso_clancash")->exists())
+        {
+            //create table structure with ...
+            $structure->table("eso_clancash")
+                //id unsigned integer not null
+                ->column("id", "int(11) unsigned", false)
+                //id as primary key
+                ->key("id", "primary")
+                //memberId int not null
+                ->column("memberId", "int(11) unsigned", false)
+                //transactionDate date not null
+                ->column("transactionDate", "date", false)
+                //description of the transaction varchar not null
+                ->column("description", "varchar(140)", false)
+                //value of payment int
+                ->column("value", "int(11)", false)
+                //do it. -- dont mind the boolean, its fine there.
+                ->exec(false);
+            return true;
+        }
     }
-
     public function disable()
     {
         return true;
     }
-
+    //function will drop the database when being uninstalled
     public function uninstall()
     {
         $structure = ET::$database->structure();
